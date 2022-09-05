39972b0165e857cafbee5d5b41df43974bd0df59
Ian Chege <107269298+TheophilusEmma@users.noreply.github.com>
2022-09-05 11:28:57 +0300

    Add files via upload

---
+++ control_flow.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+//function main begins program execution
+int main()
+{
+    int number1=0;
+    int number2=0;
+
+    cout<<"Enter two integers to compare: ";
+    cin>> number1>> number2;
+
+    if (number1==number2)
+        cout<<number1<<" == " <<number2<<endl;
+
+    if (number1!=number2)
+        cout<<number1<< "!=" <<number2<<endl;
+
+    if (number1<number2)
+        cout<<number1<<" < " <<number2<<endl;
+
+    if (number1>number2)
+        cout<<number1<<" > " <<number2<<endl;
+
+    if (number1 <= number2)
+        cout<<number1<<" <= " <<number2<<endl;
+
+    if (number1>=number2)
+        cout<<number1<<" >= " <<number2<<endl;
+
+} //end function main
