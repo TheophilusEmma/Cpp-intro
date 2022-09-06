93774b78f2b0d88b2ebeb55e25df3f5674872adb
Ian Chege <107269298+TheophilusEmma@users.noreply.github.com>
2022-09-06 10:48:49 +0300

    Add files via upload

---
+++ defining a class member with a member function.cpp
@@ -0,0 +1,21 @@
+//define class GradeBook with a member function displayMessage,
+//create a GradeBook object, and call its displayMessage Function.
+#include <iostream>
+using namespace std;
+//GradeBook class definition
+class GradeBook
+{
+public:
+    //function that displays a welcome message to the GradeBook user
+    void displayMessage() const
+    {
+        cout << "Welcome to the Grade Book!" << endl;
+    } // end function displayMessage
+}; // end class GradeBook
+
+//function main begins program execution
+int main()
+{
+    GradeBook myGradeBook; // create a GradeBook object named myGradeBook
+    myGradeBook.displayMessage(); // call object's displayMessage function
+} //end main
